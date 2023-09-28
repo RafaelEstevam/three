557a55d91ca02486435d69d2f40b72257e8d8a34
rafaeEstevam <rafael.estevamdesign@gmail.com>
2023-09-27 23:08:41 -0300

    Adding new textures

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, PerspectiveCamera } from '@react-three/drei';
 // import Chuck from './models/chuck.model';
@@ -6,17 +6,51 @@ import { Cube } from './mesh/cube.mesh';
 import { Model } from './models/porsche/Scene'
 
 function App() {
+
+  const [image, setImage] = useState('');
+  const [image2, setImage2] = useState('');
+  const [substract, setSubstract] = useState('basic');
+
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
+  const onImageChange2 = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage2(URL.createObjectURL(event.target.files[0]));
+    }
+  }
+
+  // useEffect(() => {
+  //   console.log(image)
+  // }, [image])
+
   return (
-    <Canvas style={{ height: '100vh', width: '100%', background: "#c00" }}>
-      <directionalLight intensity={10} />
-      <directionalLight intensity={100} position={[10, 0, 100]} />
-      <Suspense fallback={null}>
-        <Cube />
-        {/* <Model color={'#fff'} color2={'#232323'}/> */}
-      </Suspense>
-      <OrbitControls />
-      {/* <PerspectiveCamera position={[50, 50, 50]} fov={100} /> */}
-    </Canvas>
+    <>
+      <select value={substract} onChange={(e) => setSubstract(e.target.value)}>
+        <option value="basic">Basic</option>
+        <option value="reflexive">Reflexive</option>
+        <option value="matte">Mate</option>
+      </select>
+      <input type="file" onChange={onImageChange} className="filetype" />
+      <input type="file" onChange={onImageChange2} className="filetype" />
+      {/* <img alt="preview image" src={image}/> */}
+      <Canvas style={{ height: '100vh', width: '100%', background: "#c00" }}>
+        <directionalLight intensity={10} />
+        <directionalLight intensity={5} position={[10, 0, 100]} />
+        {/* <spotLight intensity={100}  position={[10, 0, 100]}/> */}
+        <directionalLight intensity={5} position={[10, 0, -100]} />
+        <Suspense fallback={null}>
+          <Cube image={image} image2={image2} substract={substract} />
+          {/* <Model color={'#fff'} color2={'#232323'}/> */}
+        </Suspense>
+        <OrbitControls />
+        {/* <PerspectiveCamera position={[50, 50, 50]} fov={100} /> */}
+      </Canvas>
+    </>
+
   )
 }
 
--- src/mesh/cube.mesh.jsx
+++ src/mesh/cube.mesh.jsx
@@ -1,10 +1,92 @@
 import { useRef, useState } from 'react';
+import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber'
 import { TextureLoader, CubeTexture } from 'three';
 import cartao from '../assets/cartao.jpg'
 import nyvi from '../assets/nyvi.jpg'
 
-export function Cube(props) {
+const BasicMaterial = ({ texture, attach }) => {
+    return (
+        <meshBasicMaterial
+            attach={`material-${attach}`}
+            color={"#fff"}
+            map={texture ? texture : undefined}
+        />
+    )
+}
+
+const MatteMaterial = ({ texture, attach }) => {
+    return (
+        <meshStandardMaterial
+            attach={`material-${attach}`}
+            color={"#fff"}
+            map={texture ? texture : undefined}
+            roughness={0}
+            metalness={0}
+            emissive="#000"
+        />
+    )
+}
+
+const ReflexiveMaterial = ({ texture, attach }) => {
+    return (
+        <meshPhysicalMaterial
+            color={"#fff"}
+            attach={`material-${attach}`}
+            emissive="#000"
+            reflectivity={0.1}
+            transmission={1}
+            roughness={0}
+            metalness={0}
+            clearcoat={0}
+            clearcoatRoughness={0}
+            ior={1.2}
+            thickness={10}
+            map={texture ? texture : undefined}
+        />
+    )
+}
+
+const Reflexive = ({ texture, texture2 }) => {
+    return (
+        <>
+            <MatteMaterial attach={0} />
+            <MatteMaterial attach={1} />
+            <MatteMaterial attach={2} />
+            <MatteMaterial attach={3} />
+            <MatteMaterial attach={4} texture={texture} />
+            <MatteMaterial attach={5} texture={texture2} />
+        </>
+    )
+}
+
+const Basic = ({ texture, texture2 }) => {
+    return (
+        <>
+            <BasicMaterial attach={0} />
+            <BasicMaterial attach={1} />
+            <BasicMaterial attach={2} />
+            <BasicMaterial attach={3} />
+            <BasicMaterial attach={4} texture={texture} />
+            <BasicMaterial attach={5} texture={texture2} />
+        </>
+    )
+}
+
+const Matte = ({ texture, texture2 }) => {
+    return (
+        <>
+            <MatteMaterial attach={0} />
+            <MatteMaterial attach={1} />
+            <MatteMaterial attach={2} />
+            <MatteMaterial attach={3} />
+            <MatteMaterial attach={4} texture={texture} />
+            <MatteMaterial attach={5} texture={texture2} />
+        </>
+    )
+}
+
+export function Cube({ image, image2, substract, ...props }) {
     const meshRef = useRef()
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
@@ -14,8 +96,9 @@ export function Cube(props) {
     //     roughness: 0
     // });
     // useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-    const texture = new TextureLoader().load(cartao);
-    const texture2 = new TextureLoader().load(nyvi);
+    const texture = new TextureLoader().load(image || cartao);
+    const texture2 = new TextureLoader().load(image2 || nyvi);
+
     return (
         <mesh
             {...props}
@@ -23,11 +106,28 @@ export function Cube(props) {
             scale={active ? 1.5 : 1}
             onClick={(event) => setActive(!active)}
             onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
+            onPointerOut={(event) => setHover(false)}
+        >
+
             <boxGeometry args={[4, 2, 0.02]} />
+            {/* <ReflexiveMaterial {...{texture, texture2}} /> */}
+            {substract === 'reflexive' ? (
+                <Reflexive {...{ texture, texture2 }} />
+            ) : substract === 'matte' ? (
+                <Matte {...{ texture, texture2 }} />
+            ) : (
+                <Basic {...{ texture, texture2 }} />
+            )}
+
+            {/* <ReflexiveMaterial attach={0} />
+            <ReflexiveMaterial attach={1} />
+            <ReflexiveMaterial attach={2} />
+            <ReflexiveMaterial attach={3} />
+            <ReflexiveMaterial attach={4} texture={texture} />
+            <ReflexiveMaterial attach={5} texture={texture2}/> */}
             {/* <sphereGeometry args={[15, 64, 32]}/> */}
             {/* <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} /> */}
-            <meshPhysicalMaterial
+            {/* <meshPhysicalMaterial
                 color='#fff'
                 emissive="#000"
                 reflectivity={0.5}
@@ -40,7 +140,10 @@ export function Cube(props) {
                 thickness={10}
                 map={texture}
             // wireframe
-            />
+            /> */}
+
+
+
         </mesh>
     )
 };
